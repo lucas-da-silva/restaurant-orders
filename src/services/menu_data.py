731d894f8bd8aa59b304800ad1ec72c3033cd8ff
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-16 16:42:06 -0300

    feat: implements MenuData class to create a menu of dishes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
-# Req 3
+import pandas as pd
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        for dish_name, dish_rows in (
+            pd.read_csv(source_path).drop_duplicates().groupby("dish")
+        ):
+            price = dish_rows["price"].iloc[0]
+            dish = Dish(dish_name, price)
+            ingredients = {}
+            for row in dish_rows.to_dict("records"):
+                ingredient_name = row["ingredient"]
+                if ingredient_name not in ingredients:
+                    ingredients[ingredient_name] = Ingredient(ingredient_name)
+                ingredient = ingredients[ingredient_name]
+                recipe_amount = row["recipe_amount"]
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
+            self.dishes.add(dish)
